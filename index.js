5ea9f56af557cabcd00e81f790b34ccbfea24e15
Gabby Albrecht <galbrecht28@gmail.com>
2024-02-12 17:41:47 -0500

    Rename Paths

--- index.js
+++ index.js
@@ -102,9 +102,9 @@ class StartScreen extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('game-title', '/assets/start_screen/game_title.png');
+        this.load.image('game-title', 'assets/start_screen/game_title.png');
         this.load.image('start-button', '/assets/start_screen/start_button.png');
-        this.load.image('how-to-button', '/assets/start_screen/how_to_button.png');
+        this.load.image('how-to-button', './assets/start_screen/how_to_button.png');
     }
 
     create() {
